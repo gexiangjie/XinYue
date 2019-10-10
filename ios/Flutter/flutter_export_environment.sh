f109526c0f2bc0374e75b5809495a3a552ba7dd8
gexiangjie <920927621@qq.com>
2019-10-10 23:08:50 +0800

    add toast

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/gexiangjie/Desktop/soft/flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/gexiangjie/Desktop/android/flutter/xinyue/XinYue"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/Apple/Desktop/soft/flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Apple/Desktop/Android/xinyue/xinyue"
+export "FLUTTER_TARGET=/Users/Apple/Desktop/Android/xinyue/xinyue/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/gexiangjie/Desktop/soft/flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Apple/Desktop/soft/flutter/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
--- lib/v2ex/DemoItem.dart
+++ lib/v2ex/DemoItem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
 
 class DemoItem extends StatefulWidget {
   @override
@@ -10,34 +11,40 @@ class _DemoItem extends State<DemoItem> {
   Widget build(BuildContext context) {
     return new Container(
       child: new Card(
-        child: new FlatButton(
+          child: new FlatButton(
             onPressed: () {
               print("点我点我");
+              Toast.show("鉴权失败！", context, duration: Toast.LENGTH_LONG,
+                  gravity: Toast.CENTER);
             },
-            child: new Padding(
-              padding:
+            child: new Row(
+                new Image(),
+                new Padding(
+                  padding:
                   new EdgeInsets.only(left: 0, right: 20, top: 20, bottom: 20),
-              child: new Column(
-                children: <Widget>[
-                  new Container(
-                    child: new Text("这是一个列表",
-                        style: TextStyle(color: Colors.red, fontSize: 14),
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis),
-                    alignment: Alignment.topLeft,
-                    margin: new EdgeInsets.only(top: 0, bottom: 0),
-                  ),
-                  new Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  child: new Column(
                     children: <Widget>[
-                      _getBottomItem(Icons.star, "赵赵"),
-                      _getBottomItem(Icons.subject, "皮皮"),
-                      _getBottomItem(Icons.link, "葛葛")
+                      new Container(
+                        child: new Text("这是一个列表",
+                            style: TextStyle(color: Colors.red, fontSize: 14),
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis),
+                        alignment: Alignment.topLeft,
+                        margin: new EdgeInsets.only(top: 0, bottom: 0),
+                      ),
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          _getBottomItem(Icons.star, "赵赵"),
+                          _getBottomItem(Icons.subject, "皮皮"),
+                          _getBottomItem(Icons.link, "葛葛")
+                        ],
+                      )
                     ],
-                  )
-                ],
-              ),
-            )),
+                  ),
+                )),
+          )
+
       ),
     );
   }
